8d05917b5d020cfe3407c0339fb6fd717a0dccd5
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-20 23:13:48 +0530

    Basic Mongoose Setup And bodyParser Setup

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 
 const app = express();
 const PORT = process.env.PORT | 8080;
+
+//mongo Setup
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/soccerDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+//bodyParser Setup
+app.use(bodyParser.urlencoded({ extented: true }));
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     return res.send('Hello World')
