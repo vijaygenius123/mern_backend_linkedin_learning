ff524b0e471b33f02b17dcec2ee7045e05c8cd67
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-21 20:56:21 +0530

    Created addNewPlayer Controller

---
+++ controllers/playerController.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import { PlayerSchema } from '../models/playerModel'
+
+const Player = mongoose.model('Player', PlayerSchema)
+
+export const addNewPlayer = (req, res) => {
+    let newPlayer = new Player(req.body)
+
+    newPlayer.save((err, Player) => {
+        if (err) {
+            res.send(err)
+        }
+        else {
+            res.send(Player)
+        }
+    });
+}
